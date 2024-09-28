3fde4737076ce6b00bb37fb0819e50d6fea4f0c3
Abhi Gupta <abhiguptaofficial30@gmail.com>
2024-09-28 10:39:23 +0530

    some changes in file

--- src/InfoBox.jsx
+++ src/InfoBox.jsx
@@ -11,9 +11,9 @@ export default function InfoBox({info}) {
     const INIT_URL=
         "https://images.unsplash.com/photo-1722858343990-1604f540c15d?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 
-    let HOT_URL="https://images.unsplash.com/photo-1565309503881-b823e488227a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aG90JTIwd2VhdGhlcnxlbnwwfHwwfHx8MA%3D%3D";
-    let COLD_URL="https://images.unsplash.com/photo-1668418321923-becc3ef20077?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y29sZCUyMHdlYXRoZXJ8ZW58MHx8MHx8fDA%3D";
-    let RAIN_URL="https://images.unsplash.com/photo-1618557703025-7ec58c83581a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmFpbiUyMHdlYXRoZXJ8ZW58MHx8MHx8fDA%3D";
+    let HOT_URL="https://g4.img-dpreview.com/C583B30B6CB9494FACB7C64B2EDBA751.jpg";
+    let COLD_URL="https://th.bing.com/th/id/OIP.vwoQHA1pn1ssNfEvxHyzIQHaEK?rs=1&pid=ImgDetMain";
+    let RAIN_URL="https://th.bing.com/th/id/OIP.lz8HEPcQ-GfXUoSdV6O3VAHaEo?rs=1&pid=ImgDetMain";
 
     return (
         <div className="InfoBox">
